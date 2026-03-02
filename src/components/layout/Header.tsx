5f705bf2eee1d7159630ac9f7e9ac2e239c06430
kwonminjae7278 <__@gmail.com>
2026-03-02 15:33:04 +0900

    로고복구

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image';
+import React from 'react';
 
 export const Header = () => {
   return (
     <header className="flex items-center justify-start py-4">
-      <Image
-        src="/covi_mobile_logo.jpg?v=2026"
-        alt="COVI Mobile Logo"
-        width={120}
-        height={40}
-        style={{ height: 'auto' }}
-        className="object-contain"
-        priority
-      />
+      <h1 className="text-4xl font-black italic tracking-tighter text-slate-700">COVI</h1>
     </header>
   );
 };
