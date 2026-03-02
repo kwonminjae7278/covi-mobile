7d7d15321df5bccd9cdae45aa048273595dbfd33
kwonminjae7278 <__@gmail.com>
2026-03-02 15:22:21 +0900

    로고_재수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,8 +4,8 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-start py-4">
       <Image
-        src="/covi_logo.png"
-        alt="COVI Logo"
+        src="/covi_mobile_logo.png"
+        alt="COVI Mobile Logo"
         width={100}
         height={32}
         className="h-8 w-auto object-contain"
